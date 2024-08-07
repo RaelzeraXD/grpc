85e76256064bb46846608e8f13faee6ec73066b5
RaelzeraXD <raelsf.100@gmail.com>
2024-08-07 14:10:36 -0300

    add python grpc

---
+++ Python/client.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+import logging
+import grpc
+from pb import hello_pb2
+from pb import hello_pb2_grpc
+
+def run():
+    with grpc.insecure_channel("localhost:50051") as channel:
+        stub = hello_pb2_grpc.GreeterStub(channel)
+        response = stub.SayHello(hello_pb2.HelloRequest(name="ISRAEL"))
+    print("Greeter client received: " + response.message)
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    run()
---
+++ Python/server.py
@@ -0,0 +1,22 @@
+from concurrent import futures
+import logging
+import grpc
+from pb import hello_pb2
+from pb import hello_pb2_grpc
+
+class Greeter(hello_pb2_grpc.GreeterServicer):
+    def SayHello(self, request, context):
+        return hello_pb2.HelloReply(message="Hello, %s!" % request.name)
+
+def serve():
+    port = "50051"
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    hello_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    server.add_insecure_port("[::]:" + port)
+    server.start()
+    print("Server started, listening on " + port)
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    serve()
